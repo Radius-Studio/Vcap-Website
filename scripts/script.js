c69c68c7bcf8c0a54f3f32d8b92199992f2d83a6
JasonReyesHarris <jasonreyesharris908@gmail.com>
2024-12-05 10:02:46 -0500

    Added Api documentation

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,12 @@
 multi line comments
 for later use
 */
+
+const videoSection = document.querySelector('section')
+
+fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=15&playlistId=UU1lmbL4tbFYvSNy7G7SGDfQ&key=AIzaSyCUD45Edy_JEXZODqBQtpblSuNOE8VYfYE')
+.then(res => res.json())
+.then(data=>{
+    videoSection.innerHTML = data.items[0].snippet.title
+    console.log(data.items[0]);
+})
