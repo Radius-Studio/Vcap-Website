3ffe6e5981d70eadbd9c7c1892e622b41cfd4a03
JasonReyesHarris <jasonreyesharris908@gmail.com>
2024-12-05 11:47:09 -0500

    Added notes

--- html/script.js
+++ html/script.js
@@ -1,9 +1,3 @@
-//Single line comment
-/*This symbol is for adding
-multi line comments
-for later use
-*/
-
 const videoSection = document.querySelector('section')
 
 fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=15&playlistId=UU1lmbL4tbFYvSNy7G7SGDfQ&key=AIzaSyCUD45Edy_JEXZODqBQtpblSuNOE8VYfYE')
