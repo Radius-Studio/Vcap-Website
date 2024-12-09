25a9af5e39fb2aeaa9cb2182e046a6bb7e69d45e
JasonReyesHarris <jasonreyesharris908@gmail.com>
2024-12-09 09:01:44 -0500

    js updates

--- html/script.js
+++ html/script.js
@@ -3,12 +3,6 @@ const videoSection = document.querySelector('section')
 fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=15&playlistId=UU1lmbL4tbFYvSNy7G7SGDfQ&key=AIzaSyCUD45Edy_JEXZODqBQtpblSuNOE8VYfYE')
 .then(res => res.json())
 .then(data=>{
-    data.items.forEach(el => {
-        videoSection.innerHTML +=
-        <a href="https://www.youtube.com/watch?v=${el.snippet.resourceId.videoId}" class="yt-video">
-            <img src="${el.snippet.thumbnails.maxres}"/>
-            <h3>${el.snippet.title}</h3>
-        </a>
-    });
+    videoSection.innerHTML = data.items[0].title
     console.log(data.items[0]);
 })
