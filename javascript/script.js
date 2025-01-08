2cf84f1c3f01db180fc9ae42743baa5999aa2562
Jason Reyes Harris <JasonReyesHarris908@gmail.com>
2025-01-08 16:47:38 -0500

    javascrpt overhaul

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,6 @@
 const videoSection = document.querySelector('.yt-video-section');
 const loader = document.querySelector('.loader-box')
 
-
 setTimeout(getVideos, 3000)
 
 /* In the fetch link change array result value max result to add more videos*/
@@ -16,20 +15,14 @@ fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxR
         });
         data.items.forEach((el) => {
             videoSection.innerHTML += `
-<<<<<<< HEAD
-            <iframe class="yt-video"
-            src="https://www.youtube.com/embed/${el.snippet.resourceId.videoId}?si=AZ53OHttNtNd5_1p" alt=""
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+            <iframe 
+                class="yt-video"
+                src="https://www.youtube.com/embed/${el.snippet.resourceId.videoId}?si=qGL4rse50XIP-aF3&autoplay=0&controls=1&iv_load_policy=3&rel=0" alt="${el.snippet.title}"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
             </iframe>
             `;
-=======
-            <div>
-                <a href="https://www.youtube.com/watch?v=${el.snippet.resourceId.videoId}" class="yt-video">
-                    <img class="yt-video" src="https://i.ytimg.com/vi/${el.snippet.resourceId.videoId}/maxresdefault.jpg" />
-                </a>
-            </div>`;
->>>>>>> 2a4c86674e87bdd71a314e1de8b6d9f2fc225d3b
         });
     }).catch(err => {
         loader.style.display = 'none';
@@ -37,3 +30,5 @@ fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxR
         videoSection.innerHTML = '<h3>Sorry somthing went wrong, try again later</h3>'
     });
 }
+
+/*sandbox="allow-scripts" "allow-presentation" "allow-popups" "allow-popups-to-escape-sandbox"*/
