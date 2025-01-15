aa686040baf2690d85d20df3bcc43874f422e1f3
AJ-22a <anthonycortese22@icloud.com>
2025-01-15 11:02:01 -0500

    Added functions for rotating splash screen carousel

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,10 @@
 const videoSection = document.querySelector('.yt-video-section');
 const loader = document.querySelector('.loader-box');
 
+const images = document.querySelectorAll('#carousel img');
+const dots = document.querySelector('#dots');
+const desc = document.querySelector('#desc');
+
 setTimeout(getVideos, 3000);
 loadScript('/javascript/script0.js');
 loadScript('/javascript/script1.js');
@@ -43,4 +47,42 @@ function loadScript(url)
     head.appendChild(script);
 }
 
-/*sandbox="allow-scripts" "allow-presentation" "allow-popups" "allow-popups-to-escape-sandbox"*/
+let speed = 3000;
+let index = 0;
+let intervalID;
+
+images.forEach((image, i) => {
+    const span = document.createElement('span');
+    span.className = 'dot';
+    if (i === 0) span.classList.add('active');
+    span.addEventListener('click', () => {
+        index = i;
+        startInterval();
+        updateContent();
+    });
+    dots.appendChild(span);
+})
+
+startInterval()
+function startInterval() {
+    if (intervalID) clearInterval(intervalID);
+    intervalID = setInterval(() => {
+        index++;
+        if (index === images.length) index = 0;
+        updateContent();
+    },speed);
+}
+
+updateContent()
+function updateContent() {
+
+    images.forEach(item => item.classList.remove('active'));
+    images[index].classList.add('active');
+
+    const dots = document.querySelectorAll('.dot');
+    dots.forEach(item => item.classList.remove('active'));
+    dots[index].classList.add('active');
+
+    desc.textContent = images[index].dataset.desc;
+
+}
