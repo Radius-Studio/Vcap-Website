0f7f2403f35740a366dbf8ca85f8d24631d883f1
AJ-22a <anthonycortese22@icloud.com>
2025-02-10 08:41:51 -0500

    Hamburger menu

--- javascript/script.js
+++ javascript/script.js
@@ -59,6 +59,34 @@ function updateContent() {
 
 }
 
+/* function and variables for hamburger menu */
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+    if (menu.classList.contains("showMenu")) {
+        menu.classList.remove("showMenu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+    } else {
+        menu.classList.add("showMenu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+    }
+}
+
+hamburger.addEventListener("click", toggleMenu)
+closeIcon.style.display = "none";
+
+menuItems.forEach(
+    function(menuItem) {
+        menuItem.addEventListener("click", toggleMenu);
+    }
+)
+
 /* In the fetch link change array result value max result to add more videos*/
 function getVideos() {
 fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=3&playlistId=UU1lmbL4tbFYvSNy7G7SGDfQ&key=AIzaSyCUD45Edy_JEXZODqBQtpblSuNOE8VYfYE'
